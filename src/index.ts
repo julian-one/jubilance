877dbb8e5e1a4c403630112cb084ec01074cfc7e
Julian One <julian.roberts.one@gmail.com>
2023-06-18 22:18:36 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,16 @@ export async function main(
     const handler = new Handler();
 
     const method = event.httpMethod;
-    const resource = event.requestContext.resourcePath;
-    console.log('event received:', method, resource, event);
+    const path = event.requestContext.resourcePath;
 
-    if (method === 'POST' && resource === '/recipes')
+    if (method === 'POST' && path === '/recipes')
         return handler.postRecipes(event);
-    if (method === 'GET' && resource === '/recipes')
-        return handler.getRecipes();
-    if (method === 'GET' && resource === '/recipes/{id}')
+    if (method === 'GET' && path === '/recipes') return handler.getRecipes();
+    if (method === 'GET' && path === '/recipes/{id}')
         return handler.getRecipeById(event);
-    if (method === 'PUT' && resource === '/recipes/{id}')
+    if (method === 'PUT' && path === '/recipes/{id}')
         return handler.putRecipe(event);
-    if (method === 'DELETE' && resource === '/recipes/{id}')
+    if (method === 'DELETE' && path === '/recipes/{id}')
         return handler.deleteRecipe(event);
 
     return handler.toResult(400, 'bad request: no route');
